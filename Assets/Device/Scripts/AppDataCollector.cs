96cab0172057de2866adfadee7aeeaf07f8c0507
jiaoshiguan <jiaoshiguan@yvr.cn>
2025-04-25 10:57:27 +0800

    feat: add kiosk mode samples
    [Module]: Sample Project
    DetailInfo: 增加 kiosk mode 的示例部分

---
+++ Assets/Device/Scripts/AppDataCollector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using YVR.Core;
+
+namespace YVR.Enterprise.Device.Sample
+{
+    public class AppDataCollector : MonoBehaviour
+    {
+        public TMP_InputField startupAppIF;
+        public Toggle appCloseAbilityToggle;
+        public Toggle configurationPermissionToggle;
+
+        private void Start()
+        {
+            YVRManager.instance.hmdManager.SetPassthrough(true);
+
+            startupAppIF.onValueChanged.AddListener(t => { AppMgr.instance.startupApp = t; });
+            appCloseAbilityToggle.onValueChanged.AddListener(t => { AppMgr.instance.startupAppClosable = t; });
+            configurationPermissionToggle.onValueChanged.AddListener(t => { AppMgr.instance.configurationPermission = t; });
+        }
+
+        private void Update()
+        {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            var startupApp = AppMgr.instance.startupApp;
+            if (!startupAppIF.isFocused)
+            {
+                startupAppIF.text = startupApp;
+            }
+
+            appCloseAbilityToggle.isOn = AppMgr.instance.startupAppClosable;
+            configurationPermissionToggle.isOn = AppMgr.instance.configurationPermission;
+        }
+    }
+}
