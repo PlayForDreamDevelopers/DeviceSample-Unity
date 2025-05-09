d77fd239dfcaa72274aa20bf2a6f83681e7f0c5c
yangrongZ <zhuyangrong.yvr.cn>
2025-05-09 11:34:55 +0800

    feat: add ControllerButton Sample
    DetailInfo: 添加ControllerButton模块

---
+++ Assets/Sample-ControllerButton/ControlKey.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using YVR.Enterprise.Device;
+
+[Serializable]
+public class ControlKey
+{
+    public Color m_EnableColor = Color.white; 
+    public Color m_DisableColor = Color.gray;
+    public ControllerButtonMask mask;
+    public Button keyControlButton;
+    [HideInInspector]public bool enabled;
+    
+    public void ChangeColor()
+    {
+        keyControlButton.image.color = enabled? m_EnableColor:m_DisableColor;
+    }
+}
