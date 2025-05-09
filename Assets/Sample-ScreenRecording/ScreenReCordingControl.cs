0e32410f3068ed563a85d3c944c02db1526d78ed
yangrongZ <zhuyangrong.yvr.cn>
2025-05-09 09:46:03 +0800

    feat: add ScreenRecording Sample
    DetailInfo: 添加ScreenRecording模块

---
+++ Assets/Sample-ScreenRecording/ScreenReCordingControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using YVR.Enterprise.Device;
+
+public class ScreenReCordingControl : MonoBehaviour
+{ 
+    public Button startRecordingButton;
+    public Button stopRecordingButton;
+    void Start()
+    {
+        startRecordingButton.onClick.AddListener(() =>
+        {
+            ScreenRecordingMgr.instance.StartRecordScreen();
+        });
+        stopRecordingButton.onClick.AddListener(() =>
+        {
+            ScreenRecordingMgr.instance.StopRecordScreen();
+        });
+    }
+}
